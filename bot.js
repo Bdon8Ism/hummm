2429fb5f057a7c4ee95abc7a8bb2637aa44f1487
Hybh <44950520+Bdon8Ism@users.noreply.github.com>
2019-01-23 13:54:29 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,8 @@
+﻿﻿const {Client} = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('518343423787401217').roles.find('name', 'offical').edit({color: 'RANDOM'}) 
+    },3000);
+}).login(process.env.BOT_TOKEN);
