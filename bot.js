bc8e21f68f9c77c8da3db82c876dd39de25fa615
Hybh <44950520+Bdon8Ism@users.noreply.github.com>
2019-01-23 13:55:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,26 @@
-﻿﻿const {Client} = require('discord.js');
-const client = new Client();
+  
+ 
+const Discord = require('discord.js')
+const client = new Discord.Client();
+client.on('ready', function(){
+ console.log(`Logged in as ${client.user.tag}!`); 
+   
+  // var s = ['https://cdn.hyperdev.com/paste-me.svg?1477325869954']; // صور اضافيه
+   var s = ['483055660209012736','480169573530861578','483055655800930315'];  // صور  الي بتشغل
+    setInterval(function (){  
+    client.user.setPresence({
+ game: { 
+    type: 1,
+     url: 'https://www.twitch.tv/M07SEN',
+    name: 'im try banned ',  // الكلام الي في التوتش
+    application_id: '518176101403459594', // ايدي البوت او ايدي الحساب حقك 
+     assets: {
+         large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
+ 
+    }
+  }
+    });
+    }, 5000);//سرعه تغير الصور  
+});
+    client.login(process.env.BOT_TOKEN);
 
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.get('518343423787401217').roles.find('name', 'offical').edit({color: 'RANDOM'}) 
-    },3000);
-}).login(process.env.BOT_TOKEN);
